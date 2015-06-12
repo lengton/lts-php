73fee3b28f645e92a8d5533ace02cbc35dd94854
lengton <ton@lengtonp.com>
2015-06-12 15:02:36 -0500

    pg_flush removed -- not compatible with earlier versions of PHP. Needs 5.6

--- class/ltsDB.php
+++ class/ltsDB.php
@@ -200,13 +200,5 @@ class ltsDB extends ltsBase
         return ($this->db);
     } // dbGetResource
     
-    
-    public function flush ()
-    {
-        if ($this->db_type)
-            return (true);
-        else return (pg_flush ($this->db));
-    } // flush
-    
 } // ltsDB
 ?>
